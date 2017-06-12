ec52166e0ce203bd9e263815688d103f5ebb7a47
Max Brodheim <max.brodheim@gmail.com>
2017-06-12 17:00:17 -0400

    first server

--- tiny-server.js
+++ tiny-server.js
@@ -1,3 +1,9 @@
 const http = require('http');
 const PORT = 1337;
 const server = http.createServer();
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
+server.on('request', (request, response) => {
+  console.log('I heard that!');
+})
