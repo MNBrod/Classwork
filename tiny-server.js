e15ba27f94a721cdd765b5eed16f28eb759d589f
Max Brodheim <max.brodheim@gmail.com>
2017-06-12 17:15:32 -0400

    runs the basics

--- tiny-server.js
+++ tiny-server.js
@@ -1,9 +1,22 @@
 const http = require('http');
 const PORT = 1337;
 const server = http.createServer();
+const data = [
+  'test 0',
+  'test 1',
+  'test 2',
+  'test 3'
+]
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
-server.on('request', (request, response) => {
-  console.log('I heard that!');
+server.on('request', (req, res) => {
+  console.log('url', req.url);
+  if (req.url === '/data') {
+    res.writeHead(200, { 'Content-type': 'text/plain' });
+    res.end(data.join('\n'));
+  } else {
+    res.writeHead(200, { 'Content-type': 'text/plain' });
+    res.end('nothing to do here');
+  }
 })
